16c5cd9c1899259fe3ccb92033d77418c55ec12d
unknown <caiodefreitasfernandes@gmail.com>
2026-01-31 09:51:07 -0300

    app4.py atualizado com sucesso

--- app4.py
+++ app4.py
@@ -1,12 +1,10 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-    
-    host = 'localhost',
-    user = 'root',
-    password = 'caio12345678',
-    database = 'estoque'
-
+  host = 'localhost',
+  user = 'root',
+  password = 'caio12345678',
+  database = 'estoque'
 )
 
 mycursor = mydb.cursor()
